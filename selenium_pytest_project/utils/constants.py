47b6d52f4a5bb3a69b7c10d0442e3c1b1db02d37
ThiasRS <thiasgaming@outlook.de>
2026-05-13 10:41:20 +0200

    Datei für feste Daten erstellt

---
+++ selenium_pytest_project/utils/constants.py
@@ -0,0 +1,16 @@
+# Basis-URLs
+BASE_URL = "https://grocerymate.masterschool.com/"
+
+# Bewertungssystem
+MAX_RATING_CHARS = 500
+INVALID_RATING_CHARS = 501
+HTML_INJECTION_CODE = "<html><body><h1>Test</h1></body></html>"
+
+# Altersverifikation
+DATE_OF_BIRTH_17_YEARS = "2009-05-12"
+INVALID_DATE_FORMAT = "1993/08/24"
+
+# Versandkosten
+SHIPPING_FREE_THRESHOLD = 20.00
+LOW_VALUE_PRODUCT_PRICE = 12.00
+THRESHOLD_MINUS_ONE_CENT = 19.99
