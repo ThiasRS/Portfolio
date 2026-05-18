d18f1b670be21b59b8dc621fb579b342642e51c5
ThiasRS <thiasgaming@outlook.de>
2026-05-18 09:58:48 +0200

    Test mit Funktionen erstellt für die Prüfung ob code im Bewertungstextfeld möglich ist

--- selenium_pytest_project/pages/product_page.py
+++ selenium_pytest_project/pages/product_page.py
@@ -4,7 +4,13 @@ from pages.base_page import BasePage
 class ProductPage(BasePage):
     TEXT_AREA = (By.XPATH, "//textarea")
     ERROR_MSG = (By.XPATH, "//p[@class='error-message']")
+    INVALID_INPUT_MSG = (By.XPATH, "//div[@role='status']")
+    REVIEW_SEND_BTN = (By.XPATH, "//button[@class='new-review-btn new-review-btn-send']")
 
 
     def write_product_review(self, review_text):
         self.type(self.TEXT_AREA, review_text)
+
+
+    def click_review_send_button(self):
+        self.click(self.REVIEW_SEND_BTN)
--- selenium_pytest_project/tests/test_rating_system.py
+++ selenium_pytest_project/tests/test_rating_system.py
@@ -83,3 +83,48 @@ def test_review_text_characters(driver):
     # Prüfen ob error message erscheint
     error_message = product_page.find(product_page.ERROR_MSG)
     assert error_message.is_displayed(), "Keine Error Message erschienen."
+
+
+def test_review_text_code(driver):
+    # Zufälligen Nutzer registrieren und einloggen
+    register_and_login_new_user(driver)
+
+    # Navigieren zum Shop
+    home_page = HomePage(driver)
+    home_page.go_to_shop()
+
+    # Altersverifizierung überspringen
+    shop_page = ShopPage(driver)
+    shop_page.skip_age_verification()
+
+    # Produkt in Warenkorb reinlegen
+    shop_page.add_first_product_to_cart()
+
+    # Zum Warenkorb navigieren
+    shop_page.click_cart_button()
+
+    # Daten im Checkout Prozess ausfüllen und Produkt kaufen
+    payment_info = generate_payment_and_address_data()
+    checkout_page = CheckoutPage(driver)
+    checkout_page.fill_out_shipment_payment_information(payment_info)
+    checkout_page.click_buy_now_button()
+
+    # Wieder zum shop navigieren
+    home_page = HomePage(driver)
+    home_page.go_to_shop()
+
+    # Zur Produktpage navigieren
+    shop_page = ShopPage(driver)
+    shop_page.click_first_product()
+
+    # Bewertung mit code schreiben
+    code_text = """<span style="font-family: 'Courier New', Courier, monospace; color: red; font-size: 24px;">Hacked Font</span>"""
+    product_page = ProductPage(driver)
+    product_page.write_product_review(code_text)
+
+    # Bewertung abschicken
+    product_page.click_review_send_button()
+
+    # Prüfen ob Fehlermeldung erscheint
+    invalid_input_message = product_page.find(product_page.INVALID_INPUT_MSG)
+    assert invalid_input_message.is_displayed(), "Keine Fehlermeldung erschienen"
